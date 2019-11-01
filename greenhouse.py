abb925b8422949ded8b650f96e2d693a0cd36385
Suddhodan Katolkar <shanukatolkar@gmail.com>
2019-11-01 10:58:19 +0530

    Update greenhouse.py

--- greenhouse.py
+++ greenhouse.py
@@ -3,10 +3,11 @@ import Adafruit_DHT
 import time
 import pyfirmata
 import sys
-import urllib2
+import httplib
+import urllib
 
 channel_id = 882623
-key = '48PW4P20I7YCZREZ'
+key = '48PW4P2OI7YCZREZ'
 
 dht_pin = 16
 sensor_name= Adafruit_DHT.DHT11
@@ -16,22 +17,30 @@ it = pyfirmata.util.Iterator(board)
 it.start()
 analog_0 = board.get_pin('a:0:i')
 time.sleep(.1)
-baseURL = 'https://api.thingspeak.com/update?api_key=48PW4P20I7YCZREZ' 
+baseURL = 'https://api.thingspeak.com/update?api_key=48PW4P2OI7YCZREZ' 
 
 def fetchNuploadDat():
     
     while True:
         h, t = Adafruit_DHT.read_retry(sensor_name, dht_pin)
-        s = (1-analog_0.read())*100
-        try:
-            f = urllib2.urlopen(baseURL +"&field1=%s" % (str(h)))
-            f = urllib2.urlopen(baseURL +"&field2=%s" % (str(t)))
-            f = urllib2.urlopen(baseURL +"&field3=%s" % (str(s)))
-            f.close()
-            sleep(1)
-        except:
-            print "connection failed"
-        break
+        #s = (1-analog_0.read())*100
+        print(h)
+        print(t)
+        #print(s)
+        params = urllib.urlencode({'field1':h,'field2':t,'key':key})
+        headers = {"Content-typZZe":"application/x-www-form-urlencoded","Accept":"text/plain"}
+        conn=httplib.HTTPConnection("api.thingspeak.com:80")
+        #try:
+        conn.request("POST","/update",params,headers)
+        response = conn.getresponse()
+        print(h,t)
+        print(response.status,response.reason)
+        data = response.read()
+        conn.close()
+        time.sleep(1)
+      #  except:
+      #      print "connection failed"
+       # break
 if __name__ == "__main__":
     while True:
         fetchNuploadDat()
